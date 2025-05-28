293cdd66fc40f3c57ba351c1163e52413c03d79c
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2025-05-27 22:59:22 -0300

    Button navigation

--- force-app/main/default/lwc/approvalManagementForm/approvalManagementForm.js
+++ force-app/main/default/lwc/approvalManagementForm/approvalManagementForm.js
@@ -1,5 +1,25 @@
 import { LightningElement, api } from "lwc";
+import { NavigationMixin } from "lightning/navigation";
 
-export default class ApprovalManagementForm extends LightningElement {
+export default class ApprovalManagementForm extends NavigationMixin(
+  LightningElement
+) {
   @api projectCreatedInfo;
+
+  handleViewProject() {
+    console.log(this.projectCreatedInfo.projectId);
+
+    this[NavigationMixin.Navigate]({
+      type: "standard__recordPage",
+      attributes: {
+        recordId: this.projectCreatedInfo.projectId,
+        objectApiName: "Project__c",
+        actionName: "view"
+      }
+    });
+  }
+
+  handleCreateAnother() {
+    window.location.reload();
+  }
 }
